fbcc30d73106a3503f41b0f9c9b063da6047bdf3
bmouritz <39233162+bmouritz@users.noreply.github.com>
2021-12-30 12:47:00 +1100

    new comp class

---
+++ Source/newCompressor.cpp
@@ -0,0 +1,137 @@
+/*
+  ==============================================================================
+
+    newCompressor.cpp
+    Created: 30 Dec 2021 11:08:25am
+    Author:  bmour
+
+  ==============================================================================
+*/
+
+#include "newCompressor.h"
+
+template <typename SampleType>
+newCompressor<SampleType>::newCompressor()
+{
+    update();
+}
+
+//==============================================================================
+template <typename SampleType>
+void newCompressor<SampleType>::setThreshold(SampleType newThreshold)
+{
+    thresholddB = newThreshold;
+    update();
+}
+
+template <typename SampleType>
+void newCompressor<SampleType>::setRatio(SampleType newRatio)
+{
+    jassert(newRatio >= static_cast<SampleType> (1.0));
+
+    ratio = newRatio;
+    update();
+}
+
+template <typename SampleType>
+void newCompressor<SampleType>::setAttack(SampleType newAttack)
+{
+    attackTime = newAttack;
+    update();
+}
+
+template <typename SampleType>
+void newCompressor<SampleType>::setKneeWidth(SampleType newKnee)
+{
+    kneeWidth = newKnee;
+    update();
+}
+
+template <typename SampleType>
+void newCompressor<SampleType>::setRelease(SampleType newRelease)
+{
+    releaseTime = newRelease;
+    update();
+}
+
+//==============================================================================
+template <typename SampleType>
+void newCompressor<SampleType>::prepare(const juce::dsp::ProcessSpec& spec)
+{
+    jassert(spec.sampleRate > 0);
+    jassert(spec.numChannels > 0);
+
+    sampleRate = spec.sampleRate;
+
+    envelopeFilter.prepare(spec);
+
+    update();
+    reset();
+}
+
+template <typename SampleType>
+void newCompressor<SampleType>::reset()
+{
+    envelopeFilter.reset();
+}
+
+//==============================================================================
+template <typename SampleType>
+SampleType newCompressor<SampleType>::processSample(int channel, SampleType inputValue)
+{
+    // Ballistics filter with peak rectifier
+    auto env = envelopeFilter.processSample(channel, inputValue);
+
+    float x_db = juce::Decibels::gainToDecibels(env);
+
+    if (x_db > (thresholddB + kneeWidth / 2)) {
+        // Above knee curve
+        g_sc = thresholddB + ((x_db - thresholddB) / ratio);
+    }
+    else if (x_db > (thresholddB - kneeWidth / 2)) {
+        // Within knee curve
+        g_sc = x_db + (((1 / ratio - 1) * (std::pow((x_db - thresholddB + kneeWidth / 2), 2))) / (2 * kneeWidth));
+    }
+    else {
+        // Do not compress
+        g_sc = x_db;
+    }
+
+    return  std::pow(10, g_sc * 0.05);
+}
+
+template <typename SampleType>
+SampleType newCompressor<SampleType>::processSampleStereo(float env)
+{
+    float x_db = juce::Decibels::gainToDecibels(env);
+
+    if (x_db > (thresholddB + kneeWidth / 2)) {
+        // Above knee curve
+        g_sc = thresholddB + ((x_db - thresholddB) / ratio);
+    }
+    else if (x_db > (thresholddB - kneeWidth / 2)) {
+        // Within knee curve
+        g_sc = x_db + (((1 / ratio - 1) * (std::pow((x_db - thresholddB + kneeWidth / 2), 2))) / (2 * kneeWidth));
+    }
+    else {
+        // Do not compress
+        g_sc = x_db;
+    }
+
+    return  std::pow(10, g_sc * 0.05);
+}
+
+template <typename SampleType>
+void newCompressor<SampleType>::update()
+{
+    threshold = Decibels::decibelsToGain(thresholddB, static_cast<SampleType> (-200.0));
+    thresholdInverse = static_cast<SampleType> (1.0) / threshold;
+    ratioInverse = static_cast<SampleType> (1.0) / ratio;
+
+    envelopeFilter.setAttackTime(attackTime);
+    envelopeFilter.setReleaseTime(releaseTime);
+}
+
+//==============================================================================
+template class newCompressor<float>;
+template class newCompressor<double>;
---
+++ Source/newCompressor.h
@@ -0,0 +1,122 @@
+/*
+  ==============================================================================
+
+    newCompressor.h
+    Created: 30 Dec 2021 11:08:25am
+    Author:  bmour
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+template <typename SampleType>
+class newCompressor
+{
+public:
+    //==============================================================================
+    /** Constructor. */
+    newCompressor();
+
+    //==============================================================================
+    /** Sets the threshold in dB of the compressor.*/
+    void setThreshold(SampleType newThreshold);
+
+    /** Sets the ratio of the compressor (must be higher or equal to 1).*/
+    void setRatio(SampleType newRatio);
+
+    /** Sets the attack time in milliseconds of the compressor.*/
+    void setAttack(SampleType newAttack);
+
+    /** Sets the release time in milliseconds of the compressor.*/
+    void setRelease(SampleType newRelease);
+
+    /** Sets the width of the knee of the compressor.*/
+    void setKneeWidth(SampleType newKnee);
+
+    //==============================================================================
+    /** Initialises the processor. */
+    void prepare(const juce::dsp::ProcessSpec& spec);
+
+    /** Resets the internal state variables of the processor. */
+    void reset();
+
+    //==============================================================================
+    /** Processes the input and output samples supplied in the processing context. */
+    template <typename ProcessContext>
+    void process(const ProcessContext& context) noexcept
+    {
+        const auto& inputBlock = context.getInputBlock();
+        auto& outputBlock = context.getOutputBlock();
+        const auto numChannels = outputBlock.getNumChannels();
+        const auto numSamples = outputBlock.getNumSamples();
+
+        jassert(inputBlock.getNumChannels() == numChannels);
+        jassert(inputBlock.getNumSamples() == numSamples);
+
+        if (context.isBypassed)
+        {
+            outputBlock.copyFrom(inputBlock);
+            return;
+        }
+
+        for (size_t channel = 0; channel < numChannels; ++channel)
+        {
+            auto* inputSamples = inputBlock.getChannelPointer(channel);
+            auto* outputSamples = outputBlock.getChannelPointer(channel);
+
+            for (size_t i = 0; i < numSamples; ++i)
+                outputSamples[i] = processSample((int)channel, inputSamples[i]);
+        }
+    }
+    /** Performs the processing operation on a single sample at a time. */
+    SampleType processSample(int channel, SampleType inputValue);
+
+    template <typename ProcessContext>
+    void processStereo(const ProcessContext& context) noexcept
+    {
+        const auto& inputBlock = context.getInputBlock();
+        auto& outputBlock = context.getOutputBlock();
+        const auto numChannels = outputBlock.getNumChannels();
+        const auto numSamples = outputBlock.getNumSamples();
+
+        jassert(inputBlock.getNumChannels() == numChannels);
+        jassert(inputBlock.getNumSamples() == numSamples);
+
+        if (context.isBypassed)
+        {
+            outputBlock.copyFrom(inputBlock);
+            return;
+        }
+
+        // process samples with gain amount
+        for (size_t channel = 0; channel < numChannels; ++channel)
+        {
+            auto* inputSamples = inputBlock.getChannelPointer(channel);
+            auto* outputSamples = outputBlock.getChannelPointer(channel);
+
+            for (size_t i = 0; i < numSamples; ++i) {
+                float x_max = jmax(std::abs(inputBlock.getSample(0, i)), std::abs(inputBlock.getSample(1, i)));
+                float env = envelopeFilter.processSample(channel, x_max);
+                outputSamples[i] = processSampleStereo(env);
+            }
+        }
+    }
+
+    /** Performs the processing operation on a single sample at a time. */
+    SampleType processSampleStereo(float env);
+
+private:
+    //==============================================================================
+    void update();
+
+    //==============================================================================
+    SampleType threshold, thresholdInverse, ratioInverse;
+    juce::dsp::BallisticsFilter<SampleType> envelopeFilter;
+
+    double sampleRate = 44100.0;
+    double kneeWidth = 0.0;
+    float g_sc = 0.0f;
+    SampleType thresholddB = 0.0, ratio = 1.0, attackTime = 1.0, releaseTime = 100.0;
+};
